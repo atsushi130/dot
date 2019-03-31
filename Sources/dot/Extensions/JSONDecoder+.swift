7887d36be43ccd5da0b750bec81a39d7acdc36a7
atsushi130 <atsushi_main@outlook.jp>
2019-03-31 21:00:34 +0900

    add: JSONDecoder extension

--- Sources/dot/Extensions/JSONDecoder+.swift
+++ Sources/dot/Extensions/JSONDecoder+.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension JSONDecoder {
+    static var snakeCaseDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+}
