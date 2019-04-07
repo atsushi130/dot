a495b217c4bca58e62e1557099b9ff90e9d7cabb
atsushi130 <atsushi_main@outlook.jp>
2019-04-08 00:12:46 +0900

    fix: date

--- Sources/dot/Commands/Version.swift
+++ Sources/dot/Commands/Version.swift
@@ -10,7 +10,7 @@ import Commandy
 
 enum Version: Command {
     static func run() throws {
-        print("dot v1.1.1 2019-04-07")
+        print("dot v1.1.1 2019-04-08")
         exit(EXIT_SUCCESS)
     }
 }
