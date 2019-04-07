1aca50b3d0a67daa54b24dfa781bbeb8a614ebc7
atsushi130 <atsushi_main@outlook.jp>
2019-04-08 00:05:21 +0900

    implement: dotfiles repository not found error

--- Sources/dot/Commands/DotfilesRepositoryRegister.swift
+++ Sources/dot/Commands/DotfilesRepositoryRegister.swift
@@ -11,15 +11,16 @@ import Commandy
 enum DotfilesRepositoryRegister: Command {
     static func run() throws {
         guard let dotfilesRepository = Arguments.cached.nonOptionArguments.first else {
-            throw DotfilesRepositoryRegister.Error.notFoundDotfilesRepository
+            throw DotfilesRepositoryRegister.Error.dotfilesRepositoryNotFound
         }
         UserDefaults.standard.setValue(dotfilesRepository, forKey: "GITHUB_DOTFILES_REPOSITORY")
         exit(EXIT_SUCCESS)
     }
 }
 
+// MARK: Error
 extension DotfilesRepositoryRegister {
     enum Error: Swift.Error {
-        case notFoundDotfilesRepository
+        case dotfilesRepositoryNotFound
     }
 }
