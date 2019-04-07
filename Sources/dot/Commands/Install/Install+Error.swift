5d6895a1769df0cdf4f7cd7c8915d9d18502008c
atsushi130 <atsushi_main@outlook.jp>
2019-04-08 00:09:44 +0900

    define: install error

--- Sources/dot/Commands/Install/Install+Error.swift
+++ Sources/dot/Commands/Install/Install+Error.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+extension Install {
+    enum Error: Swift.Error {
+        case filenameNotFound
+        case githubAccessTokenNotFound
+        case githubDotfilesRepositoryNotFound
+        case undefinedInDotJson(resourceName: String)
+        case undefinedChainInDotJson(resourceName: String)
+        case referenceResourceError(resourceError: GithubApi.ResourceService.Error)
+        
+        var message: String {
+            switch self {
+            case .filenameNotFound:
+                return """
+                Argument not found. Please input filename.
+                ❯ dot install filename
+                """
+            case .githubAccessTokenNotFound:
+                return """
+                Github access token not found. Please register it.
+                ❯ dot token ******
+                """
+            case .githubDotfilesRepositoryNotFound:
+                return """
+                Github dotfiles repository not found. Please register it.
+                ❯ dot repository owner/repository
+                """
+            case .undefinedInDotJson(let resourceName):
+                return """
+                Undefined \(resourceName) in dot.json. Please define dotfile information according to dot.json format.
+                document: https://github.com/atsushi130/dot#configuration
+                example:  https://github.com/atsushi130/dotfiles/blob/master/dot.json
+                """
+            case .undefinedChainInDotJson(let resourceName):
+                return "Undefined chain in \(resourceName). No chain option is required."
+            case let .referenceResourceError(resourceError):
+                return resourceError.message
+            }
+        }
+    }
+}
